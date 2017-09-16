af3428f13471176f0e86465edd28bf06f3e85a3e
ScStew <scottmstewart2@gmail.com>
2017-09-16 15:55:58 -0400

    trying to fix connection issue

--- app.rb
+++ app.rb
@@ -1,19 +1,17 @@
 require "sinatra"
-require "pony"
+
 require "pg"
-require "bundler/setup"
-require "recaptcha"
+
 load ".loval_env.rb" if File.exists?(".local_env.rb")
 
 db_params = {
-	host: ENV["host"]
-	port: ENV["port"]
-	dbname: ENV["dbname"]
-	user: ENV["user"]
+	host: ENV["host"],
+	port: ENV["port"],
+	dbname: ENV["dbname"],
+	user: ENV["user"],
 	password: ENV["password"]
 }
 
 db = PG::Connection.new(db_params)
 
-
-}
+p "#{db}"

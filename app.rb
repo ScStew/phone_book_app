8dea5c544d237c52359319f2eded9750c5b77055
ScStew <scottmstewart2@gmail.com>
2017-09-16 15:32:30 -0400

    setup git ignore and local enve

--- app.rb
+++ app.rb
@@ -5,3 +5,15 @@ require "bundler/setup"
 require "recaptcha"
 load ".loval_env.rb" if File.exists?(".local_env.rb")
 
+db_params = {
+	host: ENV["host"]
+	port: ENV["port"]
+	dbname: ENV["dbname"]
+	user: ENV["user"]
+	password: ENV["password"]
+}
+
+db = PG::Connection.new(db_params)
+
+
+}
